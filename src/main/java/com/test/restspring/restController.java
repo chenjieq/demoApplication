55e87ed59301ef5fa0d3602d0ed53a38722483b6
Jade <1019315293@qq.com>
2019-04-01 17:46:48 +0800

    can get config from config server

--- src/main/java/com/test/restspring/restController.java
+++ src/main/java/com/test/restspring/restController.java
@@ -1,8 +1,10 @@
 package com.test.restspring;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RefreshScope
 @RestController
 public class restController {
     @RequestMapping("/h")
@@ -14,4 +16,10 @@ public class restController {
     public String word() {
         return "World";
     }
+
+
+    @Value("${message:Hello default}")
+    String message;
+    @RequestMapping("/message")
+    String getMessage() { return this.message; }
 }
--- src/main/java/com/test/restspring/userServiceImpl.java
+++ src/main/java/com/test/restspring/userServiceImpl.java
@@ -1,4 +1,32 @@
 package com.test.restspring;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+import org.springframework.stereotype.Service;
+
+import org.springframework.util.Assert;
+
+
+
+@Service
+@RefreshScope
 
 public class userServiceImpl {
+
+
+
+        private final UserRepository userRepository;
+        @Autowired
+        public userServiceImpl(UserRepository userRepository) {
+            this.userRepository = userRepository;
+        }
+
+
+        //@Override
+        public User getUser(String name) {
+            Assert.notNull(name, "Name must not be null");
+            return this.userRepository.findByName(name);
+
+      }
+
 }
